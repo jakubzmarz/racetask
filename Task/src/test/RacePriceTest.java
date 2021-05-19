c038a516b4b2bff934978c3b00bdb53b0b8118b7
Kuba <kuba.zmarz@gmail.com>
2021-05-19 12:00:23 +0200

    cd

--- Task/src/test/RacePriceTest.java
+++ Task/src/test/RacePriceTest.java
@@ -52,12 +52,48 @@ public class RacePriceTest
     @Test
     public void testFourthScenario()
     {
-        RacePrice racePrice = new RacePrice(new BigDecimal(10.23444),new BigDecimal(1.444),new BigDecimal(2.555), new BigDecimal(1.342));
+        RacePrice racePrice = new RacePrice(new BigDecimal(20),new BigDecimal(0),new BigDecimal(0), new BigDecimal(0));
 
-        racePrice = PriceModifier.applyLimit(racePrice, BigDecimal.valueOf(6.999));
-        Assertions.assertEquals(BigDecimal.valueOf(4.6).setScale(2),racePrice.getBasePrice());
-        Assertions.assertEquals(BigDecimal.valueOf(0.65).setScale(2),racePrice.getTaxList().get(0));
-        Assertions.assertEquals(BigDecimal.valueOf(1.15).setScale(2),racePrice.getTaxList().get(1));
-        Assertions.assertEquals(BigDecimal.valueOf(0.6).setScale(2),racePrice.getTaxList().get(2));
+        racePrice = PriceModifier.applyLimit(racePrice, BigDecimal.valueOf(15.60));
+        Assertions.assertEquals(BigDecimal.valueOf(15.6).setScale(2),racePrice.getBasePrice());
+        Assertions.assertEquals(BigDecimal.valueOf(0).setScale(2),racePrice.getTaxList().get(0));
+        Assertions.assertEquals(BigDecimal.valueOf(0).setScale(2),racePrice.getTaxList().get(1));
+        Assertions.assertEquals(BigDecimal.valueOf(0).setScale(2),racePrice.getTaxList().get(2));
+    }
+
+    @Test
+    public void testFifthScenario()
+    {
+        RacePrice racePrice = new RacePrice(new BigDecimal(0),new BigDecimal(2),new BigDecimal(2), new BigDecimal(2));
+
+        racePrice = PriceModifier.applyLimit(racePrice, BigDecimal.valueOf(5));
+        Assertions.assertEquals(BigDecimal.valueOf(0).setScale(2),racePrice.getBasePrice());
+    }
+
+    @Test
+    public void testSixthScenario()
+    {
+        RacePrice racePrice = new RacePrice(new BigDecimal(10),new BigDecimal(-4));
+
+        racePrice = PriceModifier.applyLimit(racePrice, BigDecimal.valueOf(5));
+        Assertions.assertEquals(BigDecimal.valueOf(0).setScale(2),racePrice.getBasePrice());
+    }
+
+    @Test
+    public void testSeventhScenario()
+    {
+        RacePrice racePrice = new RacePrice(new BigDecimal(-10),new BigDecimal(24));
+
+        racePrice = PriceModifier.applyLimit(racePrice, BigDecimal.valueOf(5));
+        Assertions.assertEquals(BigDecimal.valueOf(0).setScale(2),racePrice.getBasePrice());
+    }
+
+    @Test
+    public void testEightScenario()
+    {
+        RacePrice racePrice = new RacePrice(new BigDecimal(8),new BigDecimal(1),new BigDecimal(1), new BigDecimal(1));
+
+        racePrice = PriceModifier.applyLimit(racePrice, BigDecimal.valueOf(-5));
+        Assertions.assertEquals(BigDecimal.valueOf(0).setScale(2),racePrice.getBasePrice());
     }
 }
